5ce6d12a58015ca580f1d154494be33ef1412bf3
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-11-01 23:49:48 +0100

    basic server

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[];
+
+  constructor() 
+  { 
+  }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: "first todo",
+        completed: false
+      },
+      {
+        content: "second todo",
+        completed: false
+      }
+     
+    ]
+  }
+
+}
